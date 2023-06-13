4140333e5e6e6da4e94306b090a2c03d6b6b3236
erickelsas <erickelsas@alunos.utfpr.edu.br>
2023-06-13 17:02:41 -0300

    Menu animado

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Menu from './components/Menu';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Menu style={styles.menu} selected='trophy'/>
     </View>
   );
 }
@@ -17,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  menu: {
+    flex: 1,
+  }
 });
